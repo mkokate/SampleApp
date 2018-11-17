e05aa2c47fd84d10164e5b2b23ca1aa36d8fa99b
Mahesh Kokate <mahesh@operamediaworks.com>
2018-11-17 15:06:26 +0530

    Created sample app

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Mahesh Kokate on 17/11/18.
+//  Copyright © 2018 Mahesh Kokate. All rights reserved.
+//
+
+import UIKit
+import SampleFramework
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = randomColor()
+    }
+
+}
+
